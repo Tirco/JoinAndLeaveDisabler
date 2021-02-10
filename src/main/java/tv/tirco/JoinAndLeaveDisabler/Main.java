6eb521ab936f160817656522573bc7b38eb0d0e0
Tirco <morten.liberg@gmail.com>
2021-02-10 17:10:17 +0100

    Make plugin.

--- src/main/java/tv/tirco/JoinAndLeaveDisabler/Main.java
+++ src/main/java/tv/tirco/JoinAndLeaveDisabler/Main.java
@@ -1,15 +1,16 @@
-package tv.tirco.template.spigotplugin;
+package tv.tirco.JoinAndLeaveDisabler;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpigotPlugin extends JavaPlugin {
+public class Main extends JavaPlugin {
     
     @Override
     public void onEnable() {
         // Don't log enabling, Spigot does that for you automatically!
+    	 Bukkit.getPluginManager().registerEvents(new JoinLeaveListener(), this);
 
         // Commands enabled with following method must have entries in plugin.yml
-        getCommand("example").setExecutor(new ExampleCommand(this));
     }
     
     @Override
